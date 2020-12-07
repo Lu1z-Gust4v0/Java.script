51d45fe31db87f12762f5d059e2819f3b92864ed
Luiz Gustavo Marques Costa <74384665+Lu1z-Gust4v0@users.noreply.github.com>
2020-12-06 23:08:31 -0300

    Parte 02
    Em breve começarei a realizar os desafios do curso de JavaScript

--- Aula14/ambiente.js
+++
@@ -1,5 +0,0 @@
-console.log('O começo')
-for(var c = 1;c<=5;c++){
-    console.log(`${c}`)
-} 
-console.log('O fim')
--- Aula16/function04.js
+++
@@ -1,8 +0,0 @@
-function fatorial(x=0){
-    let fat = 1
-    for(let c = x;c > 1;c--){
-        fat *= c
-    }
-    return fat 
-}
-console.log(fatorial(5))
